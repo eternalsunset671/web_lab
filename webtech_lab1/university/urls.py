6ce33c6592bc69e07304d6521716e3195c962f72
eternalsunset671 <mr.maxkozel@gmail.com>
2025-11-11 23:16:12 +1000

    lab3 is done

--- webtech_lab1/university/urls.py
+++ webtech_lab1/university/urls.py
@@ -2,17 +2,22 @@ from django.urls import path
 from . import views
 
 # TODO
-# пофиксить страницы студентов
- 
+# проверить формы регистрацию и вход ok
+# нужно ли че-то делать с енроллмент??  
+#  /course/network-defense/   error ok
+
 urlpatterns = [
-    path('', views.home_page, name='home_page'), 
+    path('', views.home_page, name='home_page'),
     path('about/', views.AboutView.as_view(), name='about'),
-    path('student/<int:student_id>/', views.student_profile, name='student'),
+    path('student/<int:pk>/', views.student_profile, name='student'),
     path('courses/', views.CourseListView.as_view(), name='courses'),
     path('course/<slug:course_slug>/', views.CourseView.as_view(), name='course'),
     
     path('feedback/', views.feedback_view, name='feedback'),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('students/', views.student_list, name='students'),
+    path('enroll/', views.enroll_view, name='enroll'),
+
 ]
